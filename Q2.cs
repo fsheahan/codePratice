ef60ca543909554871b42c1937b5cb1d11d3b273
fsheahan <fred@primeacademy.io>
2016-10-10 19:15:46 -0500

    Project Euler Q2

---
+++ Q2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+	//Project Euler Q2
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			long fib1 = 1;
+			long fib2 = 1;
+			long result = 0;
+			long summed = 0;
+
+			while (result < 4000000) {
+				if ((result % 2) == 0) {
+					summed += result;
+				}
+
+				result = fib1 + fib2;
+				fib2 = fib1;
+				fib1 = result;
+			}
+
+
+			Console.WriteLine (result);
+		}
+	}
+}
