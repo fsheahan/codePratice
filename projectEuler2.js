5140ad35b7786f61b85e93fd25edbf922c893221
fsheahan <fred@primeacademy.io>
2016-10-14 20:02:18 -0500

    Project Euler Q2 JavaScript

---
+++ projectEuler2.js
@@ -0,0 +1,36 @@
+/*
+var sum = 0;
+
+function isDivisible(num, divisor){
+  return num % divisor == 0;
+}
+
+function fib(n){
+  if (n <= 1) return(1);
+  return fib(n-1) + fib(n-2);
+}
+
+for (var i = 0; i < 10000; i++){
+  var f = fib(i);
+  if (f < 4000000) console.log(sum);
+  if (isDivisible(f, 2)){
+    sum += f;
+  }
+}
+*/
+
+function sumOfEven(limit) {
+    var temp, sum = 0, a = 0, b = 1;
+    while (b < limit) {
+        temp = a;
+        a = b;
+        b += temp;
+        if ((b & 1) === 0) {
+            sum += b;
+        }
+    }
+    return sum;
+}
+
+console.log(sumOfEven(4e6));
+*/
